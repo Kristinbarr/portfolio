81e35963e28f689602e4e8b9c880286a35502ed7
Kristin Barr <kristinbarr@gmail.com>
2019-08-21 17:30:35 -0400

    initial reformat - added 2 projects

--- src/App.js
+++ src/App.js
@@ -1,57 +1,80 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
+      <h1>Kristin Barr</h1>
+      <h2>Developer | Designer | Creator</h2>
+      <h3>Projects</h3>
+      <div className='pairme'>
         <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
+          href='https://pair-me.netlify.com/'
+          target='_blank'
+          rel='noreferrer noopener'
         >
-          ZEIT Now
+          <img src='./img/pairme-thumb.png' alt='pair me thumbnail' />
         </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/zeit/now-examples/blob/master/create-react-app-go"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
+        <p>
+          PairMe{' '}
+          <span>
+            <a
+              href='https://pair-me.netlify.com/'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              [LIVE SITE]
+            </a>{' '}
+          </span>
+          <span>
+            <a
+              href='https://youtu.be/E1mbO6ztU6c'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              [DEMO]
+            </a>
+          </span>{' '}
+        </p>
+        <p>
+          - Pair programming matchmaking app built with React, Socket.io, 2019.{' '}
+          <br />
+          - Built from scratch for 2.5-day Hackathon with 2 teammates, presented
+          app for demo video
+          <br />- Sole developer and designer for fully responsive marketing
+          website using HTML, CSS (Sass, animations), animated gifs{' '}
+        </p>
+      </div>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <a
+        href='https://kristinbarr.github.io/pic-script/'
+        target='_blank'
+        rel='noreferrer noopener'
+      >
+        <img src='./img/picscript-thumb.png' alt='pic script thumbnail' />
+      </a>{' '}
+      <p>
+        Pic Script{' '}
+        <span>
+          <a
+            href='https://kristinbarr.github.io/pic-script/'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            [LIVE SITE]
+          </a>{' '}
+        </span>{' '}
+      </p>
+      <p>
+        - Inspirational random image and quote custom pairing site
+        <br />- Project built with React(hooks), Redux, API integrations{' '}
+      </p>
+      {/* <h2>The date according to Go is:</h2>
+      <p>{date ? date : 'Loading date...'}</p> */}
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
